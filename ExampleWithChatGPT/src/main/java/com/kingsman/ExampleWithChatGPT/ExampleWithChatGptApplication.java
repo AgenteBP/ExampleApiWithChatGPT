53bc18741906809ab609c1b06f4d2d1c6ead659b
AgenteBP <104331234+AgenteBP@users.noreply.github.com>
2024-11-28 14:07:47 -0300

    28/11/2024
    Se subio el proyecto

---
+++ ExampleWithChatGPT/src/main/java/com/kingsman/ExampleWithChatGPT/ExampleWithChatGptApplication.java
@@ -0,0 +1,13 @@
+package com.kingsman.ExampleWithChatGPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleWithChatGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleWithChatGptApplication.class, args);
+	}
+
+}
